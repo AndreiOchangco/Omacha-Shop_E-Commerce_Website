8733d3023010500e3adc758f7a1c4a5904494210
Andrei Luise E. Ochangco <196190939+AndreiOchangco@users.noreply.github.com>
2025-10-05 22:48:28 +0800

    I modified several files to update the logo and icon references from a generic icon to the OmachaShop logo. The changes include updating the image source paths in the HTML files and changing the favicon link to point to the new logo.

--- Admin/public/index.php
+++ Admin/public/index.php
@@ -14,9 +14,9 @@
   <title>Omacha Shop | Dashboard</title>
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
   <link rel="stylesheet" href="./assets/css/tailwind.output.css" />
-  <link rel="stylesheet" href="./Fontend/css/main.css" />
+  <link rel="stylesheet" href="../../Fontend/css/main.css" />
   <!-- link icon -->
-  <link rel="icon" type="image/png" href="../images/icon.png" />
+  <link rel="icon" type="image/png" href="../../Fontend/images/Omacha-Shop_3000x3000/OmachaShop-Logo2.png" />
   <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
   <script src="./assets/js/init-alpine.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" />
@@ -35,7 +35,7 @@
       <div class="py-4 text-gray-500 dark:text-gray-400">
         <!-- Logo desktop -->
         <a href="index.php" class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="index.php">
-          <h1 class="m-0 text-primary1 "><span class="text-dark1"><img class="Imagealignment" src="../../Fontend/images/icon.png">Omacha</h1>
+          <h1 class="m-0 text-primary1 "><span class="text-dark1"><img class="Imagealignment" src="../../Fontend/images/Omacha-Shop_3000x3000/OmachaShop-Logo2.png">Omacha</h1>
         </a>
         
         <ul class="mt-6">
