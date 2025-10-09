ba0fc9939204f00f21b6337805dae1061b73ceb7
Andrei Luise E. Ochangco <196190939+AndreiOchangco@users.noreply.github.com>
2025-10-10 00:16:11 +0800

    g

--- Fontend/about.php
+++ Fontend/about.php
@@ -1328,20 +1328,20 @@ if ($result->num_rows > 0) {
 		// 	});
 		// });
 
-		const btn = document.querySelector(".btn-toggle");
-		const theme = document.querySelector("#theme-link");
+		//const btn = document.querySelector(".btn-toggle");
+		//const theme = document.querySelector("#theme-link");
 
 		// Lắng nghe sự kiện click vào button
-		btn.addEventListener("click", function () {
+		//btn.addEventListener("click", function () {
 			// Nếu URL đang là "ligh-theme.css"
-			if (theme.getAttribute("href") == "light-theme.css") {
+			//if (theme.getAttribute("href") == "light-theme.css") {
 				// thì chuyển nó sang "dark-theme.css"
-				theme.href = "dark-theme.css";
-			} else {
+				//theme.href = "dark-theme.css";
+			//} else {
 				// và ngược lại
-				theme.href = "light-theme.css";
-			}
-		});
+				//theme.href = "light-theme.css";
+			//}
+		//});
 	</script>
 	<!--===============================================================================================-->
 	<script src="js/main.js"></script>
