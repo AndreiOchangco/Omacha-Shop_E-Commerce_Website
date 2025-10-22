448e4d80260ec0ba1e04e4c140278d951e6ced0f
Andrei Ochangco <196190939+AndreiOchangco@users.noreply.github.com>
2025-10-22 11:03:28 +0800

    j

--- Fontend/register.php
+++ Fontend/register.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
-    <title>Omacha Shop | Sign Up</title>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Omacha Shop | Sign Up</title>
     <link rel="stylesheet" href="css/auth-form.css">
     <!-- link icon -->
     <link rel="icon" type="image/png" href="images/Omacha-Shop_3000x3000/OmachaShop-Logo2.png" />
@@ -10,7 +11,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
     <div class="auth-container">
