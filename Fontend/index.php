65e871d478d276f0cb805067e438a0c254e70b17
Andrei Ochangco <196190939+AndreiOchangco@users.noreply.github.com>
2025-11-03 20:27:21 +0800

    j

--- Fontend/index.php
+++ Fontend/index.php
@@ -389,7 +389,7 @@ if ($query->num_rows > 0) {
 					<div class="menu-desktop">
 						<ul class="main-menu">
 							<li class="active-menu">
-								<a class="index.php" href="#go-up">Home</a>
+								<a class="darkModetxt" href="#go-up">Home</a>
 								<ul class="sub-menu darkModebg-black">
 									<li><a class="darkModetxt" href="#shop-by-category">Categories</a></li>
 									<li><a class="darkModetxt" href="#new-arrivals">Arrivals</a></li>
