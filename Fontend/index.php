7bd95c3c02a3693bb3fa2b184184056e383589da
Lone-collab <louisricardoservito@gmail.com>
2025-11-21 13:57:47 +0800

    gwesv

--- Fontend/index.php
+++ Fontend/index.php
@@ -912,12 +912,12 @@ if ($query->num_rows > 0) {
 		<div class="container text-center">
 			<h1 class="text-primary1 m-t-50 noselect">Shop by Category</h1>
 			<div class="row">
-				<div class="col-md-2 col-sm-4 p-b-30 m-lr-auto noselect">
+				<div class="col-md-2 col-sm-4 p-b-30 m-lr-auto  noselect">
 					<!-- Block1 -->
 					<div style="background-color: #fff;" class="block1 wrap-pic-w noselect">
 						<a href="./Products/stuffed-animal-products.php">
 						<img class="category noselect" src="images/Homepage-images/toysy.png" alt="category-img" >
-						<h5 class="stext-121 lightModetxt-omacha darkModehyperlink noselect">Stuffed Animals</h5>
+						<h2 class="stext-121 lighttext darkModetxt noselect">Stuffed Animals</h2>
 						</a>
 					</div>
 				</div>
@@ -927,7 +927,7 @@ if ($query->num_rows > 0) {
 					<div style="background-color: #fff;" class="block1 wrap-pic-w noselect">
 						<a href="./Products/fantasy-animal-products.php" >
 						<img class="category noselect" src="images/Homepage-images/unicorn.png" alt="category-img">
-						<h5 class="stext-121 lightModetxt-omacha darkModehyperlink noselect">Fantasy Animals</h5>
+						<h5 class="stext-121 lightModetxt-omacha darkModehyperlink-omacha noselect">Fantasy Animals</h5>
 						</a>
 					</div>
 				</div>
